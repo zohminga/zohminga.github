ee753c8281a0d33dab1826c2f64e1055b591af90
zohminga <fibonacci1920@gmail.com>
2020-01-30 20:14:21 +0530

    ghdg

---
+++ build/precache-manifest.00d116632b65c2cf26e47608af5c7cff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c698cd40bcb40054feddf612182263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ea886189846f3c75328",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2648e8e4ae67fc28d52c",
+    "url": "/static/css/main.d4443cd3.chunk.css"
+  },
+  {
+    "revision": "4ea886189846f3c75328",
+    "url": "/static/js/2.37296ba2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.37296ba2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2648e8e4ae67fc28d52c",
+    "url": "/static/js/main.148f28ae.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
